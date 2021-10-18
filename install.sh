16eac8a1d7e9cb459cc22475927991f00f56ebeb
toleksa <toleksa@gmail.com>
2021-10-18 20:26:37 +0000

    install app of apps

--- install.sh
+++ install.sh
@@ -7,5 +7,17 @@ fi
 
 . ./env
 
+#CMD="helm install --create-namespace --namespace argocd --set server.ingres.enabled=true --set server.ingress.hosts=argocd.$KUBERNETES_DOMAIN argocd argo-cd/argo-cd"
+CMD="helm install --create-namespace --namespace argocd argocd argo-cd/argo-cd"
+echo $CMD
+eval $CMD
+
+# bcrypt(password)=$2a$10$rRyBsGSHK6.uc8fntPwVIuLVHgsAhAX7TcdrqW/RADU0uh7CaChLa
+kubectl -n argocd patch secret argocd-secret \
+  -p '{"stringData": {
+    "admin.password": "$2a$10$rRyBsGSHK6.uc8fntPwVIuLVHgsAhAX7TcdrqW/RADU0uh7CaChLa",
+    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+  }}'
+
 envsubst < kube-argo.yaml | kubectl apply -f -
 
